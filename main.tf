533e15421ed037f63ee6785ba5eb193b7f95a58d
Sanku Sinha <sanku@sparce.co.uk>
2020-10-28 23:46:23 +0530

    removing variable.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = "us-west-2"
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
+  region     = "ap-southeast-1"
+  //access_key = var.AWS_ACCESS_KEY
+  //secret_key = var.AWS_SECRET_KEY
 }
 
 terraform {
