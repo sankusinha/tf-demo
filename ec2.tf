dc784c3aabcdeee079792a58158aad4a2b9fdd3f
Sanku Sinha <sanku@sparce.co.uk>
2020-10-29 17:02:33 +0530

    delete ec2

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,5 @@
-resource "aws_instance" "myec2" {
-  ami = "ami-0cd31be676780afa7"
-  instance_type = "t2.micro"
-}
+#resource "aws_instance" "myec2" {
+#  ami = "ami-0cd31be676780afa7"
+#  instance_type = "t2.micro"
+#}
+#
