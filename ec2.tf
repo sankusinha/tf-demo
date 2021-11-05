93c3f99bfc102d01c3fef122822800624c94807c
Sanku <sanku@sparce.co.uk>
2021-11-05 11:48:38 -0700

    test change

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,6 @@
+//*****************************************/
 #resource "aws_instance" "myec2" {
 #  ami = "ami-0cd31be676780afa7"
-#  instance_type = "t2.micro"
+#  instance_type = "t2.micro" 
 #}
 #
